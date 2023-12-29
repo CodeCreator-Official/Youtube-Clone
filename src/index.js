ea10c82daae5b8e5cfd2948a3db8a8452a508da0
CodeCreator-Official <sankalppimpalkar23@gmail.com>
2023-12-30 01:23:16 +0530

    added user and video models

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import connectDB from './db/index.js'
+import app from './app.js'
 import dotenv from 'dotenv'
 dotenv.config({path: '../env'})
+const port = process.env.PORT || 8000
 
-connectDB() 
-.catch((err) => console.log(err.message))
+connectDB()
+.then(() => {
+    // Handling error on app
+    app.on('error', (err) => {
+        console.log(err)
+    })
+
+    app.listen(port, () => {
+        console.log(`Server is listening on http://localhost:${port}`)
+    })
+})
+.catch((err) => console.log("MONGO DB CONNECTION FAILED",err.message))
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+// Using try catch
+// const asyncHandler = (fn) =>
+//     async (req, res, next) => {
+//         try {
+//             await fn(req, res, next)
+//         } catch (error) {
+//             res
+//                 .status(error.code || 500)
+//                 .json({
+//                     success: false,
+//                     message: error.message,
+//                 })
+//         }
+//     }
+
+// Using Promises
+const asyncHandler = (reqHandler) => {
+    (req, res, next) => {
+        Promise
+            .resolve(reqHandler(req, res, next))
+            .catch((error) => {
+                next(error)
+            })
+    }
+}
+
+export default asyncHandler
